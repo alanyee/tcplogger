3d980c19944320d312a656be1d3770b363cd09d6
Alan Yee <alyee@ucsd.edu>
2018-04-04 23:03:22 -0700

    Add docstrings and comments

--- ids.py
+++ ids.py
@@ -1,3 +1,5 @@
+"""Class for cache of UIDs"""
+
 import json
 import subprocess as sp
 
@@ -6,10 +8,13 @@ ALPHA15 = "abcdefghij"
 ID = "/bin/id"
 
 class UserIDs(object):
-    def __init__(self, ids=None):
+    """Interface for handling the cache"""
+
+    def  __init__(self, ids=None):
         self.ids = ids if ids is not None else {}
 
     def load(self):
+        """Load cache, otherwise just create new cache"""
         try:
             json_file = open('ids.json')
             self.ids = json.load(json_file)
@@ -17,25 +22,30 @@ class UserIDs(object):
             self.create()
 
     def access(self, user):
+        """Access information in cache"""
         return self.ids[user].encode('utf-8')
 
     def add(self, user, uid):
+        """Adds user and uid to cache"""
         self.ids[user] = uid
 
     def have(self, user):
+        """Check if user's information is in cache"""
         return user in self.ids
 
     def clear(self):
+        """Clear cache"""
         self.ids.clear()
 
     def close(self):
+        """Safely closes and saves files"""
         id_json = json.dumps(self.ids)
         json_file = open("ids.json", "w")
         json_file.write(id_json)
         json_file.close()
-        print id_json
 
     def create(self):
+        """Prepopulate cache, eventually will remove"""
         abc = ALPHA
         for i in abc:
             if i == "h":
--- log.py
+++ log.py
@@ -1,3 +1,5 @@
+"""Logger for TCP connections by user"""
+
 import csv
 import random
 import subprocess as sp
@@ -22,25 +24,32 @@ mapped_ids.load()
 if prompt == "y":
     mapped_ids.clear()
 
+# Creates csv file
 with open(file_name, 'w') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=HEADER)
     writer.writeheader()
     try:
         while True:
+            # Saves a snapshot of TCP and UID information
             ps = sp.check_output([PS, "aux"])
             tcp = sp.check_output([CAT, "/proc/net/tcp"])
             snap = time.time()
+            # Cleans the data
             pslines = ps.splitlines()
             tcplines = tcp.splitlines()
             user = random.choice(pslines).strip().split()
+            # Ignores noise
             while user[0] == "root" or user[0] == "USER" or user[0] == "libstor+":
                 user = random.choice(pslines).strip().split()
             pid = user[1]
             user = user[0]
+            # Relies on cache first for finding user information
             if mapped_ids.have(user):
                 uid = mapped_ids.access(user)
+            # Ignores username or uid if misconfigured
             elif user in unknown_ids:
                 continue
+            # Attempts to figure out username and uid
             else:
                 try:
                     uid = sp.check_output([ID, "%s" % user])
@@ -53,9 +62,11 @@ with open(file_name, 'w') as csv_file:
                             unknown_ids.append(line[64:])
                     continue
             acts = []
+            # Finds all of a given user's active TCP connections
             for line in tcplines:
                 if line.strip().split()[7] == uid:
                     acts.append(line)
+            # Randomly assigns TCP connection to process
             if acts:
                 act = random.choice(acts).rstrip()
                 if act:
@@ -64,6 +75,7 @@ with open(file_name, 'w') as csv_file:
                                      'pid': '%s' % pid,
                                      'uid': '%s' % uid,
                                      'act': '%s' % act})
+# Saves cache and prints out unknown users upon shutdown
     except KeyboardInterrupt:
         mapped_ids.close()
         for unknown in unknown_ids:
